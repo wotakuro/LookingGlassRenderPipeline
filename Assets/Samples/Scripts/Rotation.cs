41b91de8c35c6941aa4deeab7505241a6072c1c3
wotakuro <wotakuro@gmail.com>
2019-03-30 17:48:08 +0900

    update sample

---
+++ Assets/Samples/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float angle = 360.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * 360.0f);
+    }
+}
